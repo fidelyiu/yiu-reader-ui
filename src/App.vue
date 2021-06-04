d96ccfcc554329be617ff052294091e842bee09b
Fidel_Yiu <fidel-yiu@qq.com>
2021-06-05 00:36:42 +0800

    故意出错的lint

--- src/App.vue
+++ src/App.vue
@@ -17,20 +17,20 @@
     },
     setup() {
       let a1 = 10
-      let a2 = 10
-      let a3 = 10
-      let a4 = 10
-      let a5 = 10
-      let a6 = 10
-      let a7 = 10
-      let a8 = 10
-      let a9 = 10
-      let a0 = 10
-      let a10 = 10
-      let a11 = 10
-      let a12 = 10
-      let a13 = 10
-      let a41 = 10
+      // let a2 = 10
+      // let a3 = 10
+      // let a4 = 10
+      // let a5 = 10
+      // let a6 = 10
+      // let a7 = 10
+      // let a8 = 10
+      // let a9 = 10
+      // let a0 = 10
+      // let a10 = 10
+      // let a11 = 10
+      // let a12 = 10
+      // let a13 = 10
+      // let a41 = 10
       return {}
     },
   })
