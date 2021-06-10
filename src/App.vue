6086b2229af7e0dd647969c236dc42c56e3d0288
Fidel_Yiu <fidel-yiu@qq.com>
2021-06-10 23:11:12 +0800

    添加懂底部样式

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
-  <router-view></router-view>
+  <div class="h-full flex flex-col overflow-hidden">
+    <div class="flex-grow">
+      <router-view></router-view>
+    </div>
+    <BottomBox></BottomBox>
+  </div>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue'
+  import BottomBox from '/@/components/BottomBox.vue'
 
   export default defineComponent({
     name: 'App',
+    components: { BottomBox },
   })
 </script>
 
 <style>
+  body {
+    overflow: hidden;
+  }
+
+  #app {
+    @apply h-full w-full overflow-hidden
+  }
 </style>
---
+++ src/components/BottomBox.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex-none max-h-7 h-7 bg-gray-800 text-white flex">
+    <div class="flex-none self-center mr-2">text-left</div>
+    <div class="flex-grow self-center">text-center</div>
+    <div class="flex-none self-center ml-2">text-right</div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: '',
+  })
+</script>
+
+<style scoped>
+
+</style>
