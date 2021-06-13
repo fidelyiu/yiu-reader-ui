891e408a41fe66a16dd9809e70c2ab01c1baed5f
Fidel_Yiu <fidel-yiu@qq.com>
2021-06-13 22:59:37 +0800

    无效数据提示完成

--- src/views/system/workspace-list/WorkspaceList.vue
+++ src/views/system/workspace-list/WorkspaceList.vue
@@ -15,14 +15,16 @@
                  @blur="searchActive = false">
         </div>
       </div>
+      <!--添加按钮-->
       <div class="flex-none">
-        <div class="p-2 bg-blue-100 hover:bg-blue-50">
+        <SquareButton>
           <span class="iconify block" data-icon="mdi:plus" data-inline="false"></span>
-        </div>
+        </SquareButton>
       </div>
     </div>
     <!--空数据栏-->
-    <div v-if="!workspaceListLoading && !workspaceList.length" class="absolute inset-0 mt-[32px] grid place-items-center">
+    <div v-if="!workspaceListLoading && !workspaceList.length"
+         class="absolute inset-0 mt-[32px] grid place-items-center">
       <div class="flex items-center text-gray-500">
         <span class="iconify text-2xl" data-icon="mdi:inbox" data-inline="false"></span>
         <span class="leading-10 text-base">暂无数据</span>
@@ -38,14 +40,14 @@
            :key="index"
            class="py-3 px-4 hover:bg-blue-50 flex">
         <template v-if="item.isEffective">
-        <span class="iconify block flex-none text-4xl self-center mr-4 text-blue-400"
-              data-icon="mdi:book"
-              data-inline="false"></span>
+          <span class="iconify block flex-none text-4xl self-center mr-4 text-blue-400"
+                data-icon="mdi:book"
+                data-inline="false"></span>
         </template>
         <template v-else>
-        <span class="iconify block flex-none text-4xl self-center mr-4 text-red-400"
-              data-icon="mdi:book-remove"
-              data-inline="false"></span>
+          <span class="iconify block flex-none text-4xl self-center mr-4 text-red-400"
+                data-icon="mdi:book-remove"
+                data-inline="false"></span>
         </template>
         <div class="flex-grow w-0 mr-4">
           <div class="w-full truncate text-gray-700 font-medium" :class="{'!text-red-400': !item.isEffective}">
@@ -56,10 +58,17 @@
           </div>
         </div>
         <div class="flex-none flex">
-          <div class="self-center mr-4">
-            <SquareButton>
-              <span class="iconify block" data-icon="mdi:help-circle" data-inline="false"></span>
-            </SquareButton>
+          <div v-if="!item.isEffective" class="self-center mr-4">
+            <NTooltip :style="{ maxWidth: '300px' }" placement="top">
+              <template #trigger>
+                <div>
+                  <SquareButton disable>
+                    <span class="iconify block" data-icon="mdi:help-circle" data-inline="false"></span>
+                  </SquareButton>
+                </div>
+              </template>
+              <span>该工作空间路径已失效，请将丢失的文件夹还原，或者删除该工作空间!</span>
+            </NTooltip>
           </div>
           <div class="self-center mr-4">
             <SquareButton :disable="!item.isEffective">
@@ -92,10 +101,11 @@
   import { boolGetRandom } from 'yiu-js/bool/bool-get'
   import LoadingIcon from '/@/components/LoadingIcon.vue'
   import SquareButton from '/@/components/SquareButton.vue'
+  import { NTooltip } from 'naive-ui'
 
   export default defineComponent({
     name: 'WorkspaceList',
-    components: { LoadingIcon, SquareButton },
+    components: { LoadingIcon, SquareButton, NTooltip },
     setup() {
       onMounted(() => {
         getWorkspaceList()
@@ -113,7 +123,7 @@
       const getWorkspaceList = () => {
         workspaceListLoading.value = true
         setTimeout(() => {
-          for (let i = 0; i < 0; i++) {
+          for (let i = 0; i < 50; i++) {
             workspaceList.push({
               name: 'Item' + (i + 1),
               url: 'F:\\CodeLearn\\vue3\\ant-vue1\\' + (i + 1),
