d751dd91c3e7338ac2e61830a404eb6e19369dd9
Fidel_Yiu <fidel-yiu@qq.com>
2021-06-26 18:09:51 +0800

    拖拽完成

--- src/views/dashboard/widget/Widget.vue
+++ src/views/dashboard/widget/Widget.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="p-2 absolute"
        :class="{
+         'select-none': moveState || resizeState,
          moving: moveState,
          resizing: resizeState,
        }"
-       :style="{
-         'left': (layout.left||0)+'px',
-         'top':(layout.top||0)+'px',
-         'width':(layout.width||0)+'px',
-         'height':(layout.height||0)+'px',
-       }">
+       :style="mainStyle()">
     <div class="shell">
       <!--组件内容-->
       <div class="w-full h-full overflow-auto"
@@ -23,10 +19,12 @@
       <!--修改状态-->
       <div v-if="customizeMode"
            class="absolute top-0 left-0 w-full h-full cursor-move"
+           @mousedown="onMoveStart"
            @click="selectId">
         <div class="customize-overlay"
              :class="{'!border-blue-400 customize-overlay-select': isSelectId}">
-          <button class="yiu-modal-close-btn-2" @click="delLayout">
+          <button class="yiu-modal-close-btn-2"
+                  @click.stop="delLayout">
             <div v-show="delLoading">
               <span class="iconify block animate-spin" data-icon="mdi:loading" data-inline="false"></span>
             </div>
@@ -55,13 +53,8 @@
   <!--移动状态-->
   <div v-if="moveState"
        class="move-ghost"
-       :style="{
-         'left': (layout.left||0)+'px',
-         'top':(layout.top||0)+'px',
-         'width':(layout.width||0)+'px',
-         'height':(layout.height||0)+'px',
-       }">
-    <div class="h-full w-full bg-indigo-400"/>
+       :style="moveGhostStyle()">
+    <div class="h-full w-full bg-indigo-400 opacity-60"/>
   </div>
   <!--调整大小状态-->
   <div v-if="resizeState"
@@ -72,7 +65,7 @@
          'width':(layout.width||0)+'px',
          'height':(layout.height||0)+'px',
        }">
-    <div class="h-full w-full bg-indigo-400"/>
+    <div class="h-full w-full bg-indigo-400 opacity-60"/>
   </div>
 </template>
 
@@ -84,6 +77,7 @@
   import SERVER_API from '/@/api'
   import { useNotification } from 'naive-ui'
   import { useWidgetStore } from '/@/store/modules/widget'
+  import { isNumber } from 'lodash'
 
   export default defineComponent({
     name: 'Widget',
@@ -114,10 +108,10 @@
       }
       const showResizeHandle = computed(() => {
         // 选择状态 && 非移动状态 && 可以修改大小的组件
-        return isSelectId.value
+        return isSelectId.value && !moveState.value
       })
-      const moveState = ref(false)
-      const resizeState = ref(false)
+      const moveState = ref<any>()
+      const resizeState = ref<any>()
       const resizeHandles = [
         'top-left',
         'top',
@@ -128,6 +122,84 @@
         'bottom-left',
         'left',
       ]
+      const initMousePosition = {
+        x: 0,
+        y: 0,
+      }
+      const onMoveStart = (e) => {
+        // 初始化鼠标位置
+        initMousePosition.x = e.clientX
+        initMousePosition.y = e.clientY
+        // 修改移动状态
+        updateMoveState(e)
+        window.addEventListener('mousemove', onMoveUpdate)
+        window.addEventListener('mouseup', onMoveEnd)
+      }
+      const updateMoveState = (e) => {
+        const gridSize = 50
+        const zoom = 0.75
+        // 获取此时鼠标相对于初始状态下的距离
+        const mouseDeltaX = e.clientX - initMousePosition.x
+        const mouseDeltaY = e.clientY - initMousePosition.y
+
+        // 修改组件的left和top
+        const pxX = prop.layout.left + mouseDeltaX / zoom
+        const pxY = prop.layout.top + mouseDeltaY / zoom
+
+        // 计算移动阴影的left和top
+        let x = gridSize * Math.round(pxX / gridSize)
+        let y = gridSize * Math.round(pxY / gridSize)
+        if (x < 0) x = 0
+        if (y < 0) y = 0
+        moveState.value = { pxX, pxY, x, y }
+      }
+      const onMoveUpdate = (e) => {
+        updateMoveState(e)
+      }
+      const onMoveEnd = (e) => {
+        updateMoveState(e)
+        removeMoveListeners()
+        moveState.value = undefined
+      }
+      const removeMoveListeners = () => {
+        window.removeEventListener('mousemove', onMoveUpdate)
+        window.removeEventListener('mouseup', onMoveEnd)
+      }
+      const mainStyle = () => {
+        if (moveState.value) {
+          return {
+            left: `${moveState.value.pxX || 0}px`,
+            top: `${moveState.value.pxY || 0}px`,
+            width: `${prop.layout.width || 0}px`,
+            height: `${prop.layout.height || 0}px`,
+          }
+        }
+        if (resizeState.value) {
+          return {}
+        }
+        return {
+          left: `${prop.layout.left || 0}px`,
+          top: `${prop.layout.top || 0}px`,
+          width: `${prop.layout.width || 0}px`,
+          height: `${prop.layout.height || 0}px`,
+        }
+      }
+      const moveGhostStyle = () => {
+        let x = moveState.value.x
+        let y = moveState.value.y
+        if (!isNumber(x)) {
+          x = prop.layout.left || 0
+        }
+        if (!isNumber(y)) {
+          y = prop.layout.top || 0
+        }
+        return {
+          left: `${x}px`,
+          top: `${y}px`,
+          width: `${prop.layout.width || 0}px`,
+          height: `${prop.layout.height || 0}px`,
+        }
+      }
       return {
         layoutType,
         layoutTypeIsLink,
@@ -139,6 +211,9 @@
         resizeHandles,
         moveState,
         resizeState,
+        onMoveStart,
+        mainStyle,
+        moveGhostStyle,
       }
     },
   })
