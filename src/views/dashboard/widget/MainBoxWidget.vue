0e37096e81d6e34e06a620ca96137aa62b548f7c
Fidel_Yiu <fidel-yiu@qq.com>
2021-07-04 09:51:15 +0800

    笔记刷新Api测试成功

--- src/views/dashboard/widget/MainBoxWidget.vue
+++ src/views/dashboard/widget/MainBoxWidget.vue
@@ -2,7 +2,7 @@
   <div class="relative w-full h-full overflow-hidden py-3 pr-1">
     <!--右上角按钮组-->
     <div class="absolute top-2 right-2">
-      <button class="yiu-blue-square-btn-5">
+      <button class="yiu-blue-square-btn-5" @click="onTest">
         <span class="iconify block" data-icon="mdi:cog" data-inline="false"></span>
       </button>
     </div>
@@ -21,6 +21,27 @@
     name: 'MainBoxWidget',
     props: {
       layout: propTypes.object.isRequired,
+    },
+    setup() {
+      const onTest = () => {
+        const ws = new WebSocket('ws://localhost:8081/workspace/refresh')
+        //连接打开时触发
+        ws.onopen = () => {
+          console.log('Connection open ...')
+          ws.send('Hello WebSockets!')
+        }
+        //接收到消息时触发
+        ws.onmessage = (evt) => {
+          console.log('Received Message: ' + evt.data)
+        }
+        //连接关闭时触发
+        ws.onclose = () => {
+          console.log('Connection closed.')
+        }
+      }
+      return {
+        onTest,
+      }
     },
   })
 </script>
